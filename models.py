548e0a6d0a343e9ee39cbc58a73fa1c73faaa44e
workingpleasewait <ziuYCoA1FV7vpFHxkNP2O5hFmt0@pm.me>
2024-09-11 03:53:38 +0000

    v.1

--- models.py
+++ models.py
@@ -11,3 +11,17 @@ class Subscriber(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     full_name: Mapped[str] = mapped_column(String(100))
     email: Mapped[str] = mapped_column(String(120), unique=True)
+
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model, UserMixin):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(64), unique=True)
+    password_hash: Mapped[str] = mapped_column(String(1024))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
